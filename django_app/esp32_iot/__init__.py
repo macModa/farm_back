c8eb781ad0715c02fba651860e3cb50732a2316b
JesseLee <jesssser04@gmail.com>
2025-10-10 17:43:12 +0100

    Update __init__.py

--- django_app/esp32_iot/__init__.py
+++ django_app/esp32_iot/__init__.py
@@ -1,9 +1,12 @@
-# This will make Python treat the directories as containing packages
-from mongoengine import connect
-import certifi
+from mongoengine import connect, disconnect
 
+# Déconnecte si une connexion existe déjà
+disconnect(alias='default')
+
+# Reconnecte proprement
 connect(
-    host="mongodb+srv://jesssser93_db_user:FydCbJAO4CqguvLu@cluster0.5y36wng.mongodb.net/farmdb?retryWrites=true&w=majority&tls=true",
-    tlsCAFile=certifi.where()
+    db='esp32_iot_data',
+    host='mongodb+srv://jesssser93_db_user:FydCbJAO4CqguvLu@cluster0.5y36wng.mongodb.net/farmdb?retryWrites=true&w=majority&tls=true'
 )
+
 print("✅ MongoDB connected successfully!")
